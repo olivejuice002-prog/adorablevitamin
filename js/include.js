cea6712aff2572448e9d95f3d48837d9455912e8
vitamin <olivejuice002@gmail.com>
2026-07-21 22:53:16 +0900

    idnclude js수정

--- js/include.js
+++ js/include.js
@@ -1,12 +1,27 @@
-window.addEventListener('DOMContentLoaded', () => {
-    // 주소 앞에 /를 붙이면 항상 루트(가장 바깥) 폴더부터 시작합니다.
-    if (document.getElementById('header-html')) {
-        fetch('/include/header.html').then(res => res.text()).then(data => document.getElementById('header-html').innerHTML = data);
+window.addEventListener("DOMContentLoaded", async () => {
+
+    async function load(id, file) {
+
+        const target = document.getElementById(id);
+
+        if (!target) return;
+
+        const res = await fetch(file);
+
+        target.innerHTML = await res.text();
+
     }
-    if (document.getElementById('subHead-html')) {
-        fetch('/include/subHead.html').then(res => res.text()).then(data => document.getElementById('subHead-html').innerHTML = data);
+
+    await load("header-html", "/include/header.html");
+
+    if (typeof initMenu === "function") {
+
+        initMenu();
+
     }
-    if (document.getElementById('footer-html')) {
-        fetch('/include/footer.html').then(res => res.text()).then(data => document.getElementById('footer-html').innerHTML = data);
-    }
+
+    await load("subHead-html", "/include/subHead.html");
+
+    await load("footer-html", "/include/footer.html");
+
 });
--- js/main.js
+++ js/main.js
@@ -1,49 +1,6 @@
-
-
-/*modal*/
-$(function() {
-	$('#sub .list .cont .thumb').on('click',function() {
-		$('.modal').css({'display':'block'});
-		$('.modal .bg').css({'background':'#000','opacity':'0.9'});
-
-		var img = $(this).find('img').attr('src');
-		$('.modal').find('img').attr('src',img);
-
-		var w = $('.modal img').outerWidth() / 2;
-		var h = $('.modal img').outerHeight() / 2;
-
-		$('.modal img').css({'margin-left':-w, 'margin-top':-h});
-	});
-
-	$('.modal .xi-close').on('click',function() {
-		$('.modal').css({'display':'none'});
-	});
-});
-
-
-
-/*modal*/
-$(function() {
-	$('#sub .list .thumb2').on('click',function() {
-		$('.modal').css({'display':'block'});
-		$('.modal .bg').css({'background':'#000','opacity':'0.9'});
-
-		var img = $(this).find('img').attr('src');
-		$('.modal').find('img').attr('src',img);
-
-		var w = $('.modal img').outerWidth() / 2;
-		var h = $('.modal img').outerHeight() / 2;
-
-		$('.modal img').css({'margin-left':-w, 'margin-top':-h});
-	});
-
-	$('.modal .xi-close').on('click',function() {
-		$('.modal').css({'display':'none'});
-	});
-});
-
-
-
+/* ==========================
+   MENU
+========================== */
 
 function initMenu() {
 
@@ -54,58 +11,118 @@ function initMenu() {
 
     menu.hover(function () {
 
-        $(this).find(".submenu").stop().slideDown();
-        $(".sub_bg").stop().slideDown();
+        $(this).find(".submenu").stop().slideDown(200);
+        $(".sub_bg").stop().slideDown(200);
 
     }, function () {
 
-        $(this).find(".submenu").stop().slideUp();
-        $(".sub_bg").stop().slideUp();
+        $(this).find(".submenu").stop().slideUp(200);
+        $(".sub_bg").stop().slideUp(200);
 
     });
 
 }
 
 
-$(document).ready(function(){
-	  $('.bxslider').bxSlider({
-	  auto: true,
-	  speed: 1000,
-	  pause: 4000,
-	  mode:'horizontal',
-	 });
- });
+/* ==========================
+   MODAL
+========================== */
 
+function initModal() {
 
-$(document).ready(function(){
+    $('#sub .list .cont .thumb, #sub .list .thumb2').on('click', function () {
 
-	   moveProgressBar();
-    // on browser resize...
-    $(window).resize(function() {
-        moveProgressBar();
+        $('.modal').show();
+
+        $('.modal .bg').css({
+            background: '#000',
+            opacity: 0.9
+        });
+
+        var img = $(this).find('img').attr('src');
+
+        $('.modal img').attr('src', img);
+
+        var w = $('.modal img').outerWidth() / 2;
+        var h = $('.modal img').outerHeight() / 2;
+
+        $('.modal img').css({
+            marginLeft: -w,
+            marginTop: -h
+        });
+
     });
 
-    // SIGNATURE PROGRESS
+    $('.modal .xi-close').on('click', function () {
+
+        $('.modal').hide();
+
+    });
+
+}
+
+
+/* ==========================
+   BXSLIDER
+========================== */
+
+function initSlider() {
+
+    $('.bxslider').bxSlider({
+
+        auto: true,
+        speed: 1000,
+        pause: 4000,
+        mode: 'horizontal'
+
+    });
+
+}
+
+
+/* ==========================
+   PROGRESS BAR
+========================== */
+
+function initProgress() {
+
+    moveProgressBar();
+
+    $(window).resize(function () {
+
+        moveProgressBar();
+
+    });
+
     function moveProgressBar() {
-      console.log("moveProgressBar");
+
         var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+
         var getProgressWrapWidth = $('.progress-wrap').width();
+
         var progressTotal = getPercent * getProgressWrapWidth;
-        var animationLength = 2000;
 
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
         $('.progress-bar').stop().animate({
+
             left: progressTotal
-        }, animationLength);
+
+        }, 2000);
+
     }
+
+}
+
+
+/* ==========================
+   PAGE INIT
+========================== */
+
+$(function () {
+
+    initSlider();
+
+    initModal();
+
+    initProgress();
+
 });
-
-
-
-
-
-
-
-
-
